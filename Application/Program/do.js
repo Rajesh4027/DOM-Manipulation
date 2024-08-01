be6f349a04031da3b476196e20c86c9981c15ac5
Rajesh4027 <rajeshdevaraj44@gmail.com>
2024-08-01 12:01:33 +0530

    dom cart app

---
+++ Application/Program/do.js
@@ -0,0 +1,31 @@
+let qty = document.getElementById('quantity')
+let minus = document.getElementById('minus')
+let plus = document.getElementById('plus')
+let totalcount = document.getElementById('total')
+
+
+let valuecount = 0;
+
+
+qty.textContent = 0
+plus.addEventListener('click',()=>{
+    let add = ++valuecount;
+    qty.textContent = valuecount
+    totalcount.innerHTML = add*199+" &#8377"
+})
+        
+
+minus.addEventListener('click',()=>{
+    if(valuecount>0){
+        let sub = --valuecount;
+        qty.textContent = valuecount
+        totalcount.innerHTML = sub*199+" &#8377" 
+    }
+})
+
+    
+
+
+
+
+
